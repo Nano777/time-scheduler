9634f0fe86a26b80fe40712960e40f4ca6b06ea0
unknown <tatata7956@icloud.com>
2018-11-04 02:06:38 +0900

    my1sttime

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-// import modules
-const express = require('express')
-const request = require('request')
-const bodyParser = require('body-parser')
+'use strict';
 
-// create a new express server
-const app = express()
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({
-  extended: true
-})) // for parsing application/x-www-form-urlencoded
+const config = {
+    channelSecret: 'b893f338979de6fd50e02a8d97ae3cfb',
+    channelAccessToken: 'CaG10hTyBbHkVLRtLZ4B7NCoQ2VkTr7KxlmggfXD3yZvwSP2jD6BjtuTq4I73CLmigmn8q45BO5pZNaXXJQ/jwqKR0dbzoVoVRxddSmCXUe8spQ4H4ji8FDn15+RFBXxNu7aFR8LfGSqUeSV5jeaBgdB04t89/1O/w1cDnyilFU='
+};
 
-app.post('/callback', (req, res) => {
-  const options = {
-	method: 'POST',
-	uri: 'https://api.line.me/v2/bot/message/reply',
-	body: {
-	  replyToken: req.body.events[0].replyToken,
-	  messages: [{
-		type: 'text',
-		text: req.body.events[0].message.text // ここに指定した文字列がボットの発言になる
-	  }]
-	},
-	auth: {
-	  bearer: 'CaG10hTyBbHkVLRtLZ4B7NCoQ2VkTr7KxlmggfXD3yZvwSP2jD6BjtuTq4I73CLmigmn8q45BO5pZNaXXJQ/jwqKR0dbzoVoVRxddSmCXUe8spQ4H4ji8FDn15+RFBXxNu7aFR8LfGSqUeSV5jeaBgdB04t89/1O/w1cDnyilFU=
-' // ここは自分のtokenに書き換える
-	},
-	json: true
+const app = express();
+
+app.post('/callback', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
   }
-  request(options, (err, response, body) => {
-	console.log(JSON.stringify(response))
-  })
-  res.send('OK')
-})
 
-app.listen(process.env.PORT || 200, () => {
-  console.log('server starting on PORT:' + process.env.PORT)
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text //実際に返信の言葉を入れる箇所
+  });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
